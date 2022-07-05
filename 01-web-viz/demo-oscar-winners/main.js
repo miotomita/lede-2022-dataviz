379ecfdd816807b478c4c0b1892710511e963f2a
miotomita <mio.tomita@nex.nikkei.com>
2022-07-05 12:40:36 -0400

    Update main.js

--- 01-web-viz/demo-oscar-winners/main.js
+++ 01-web-viz/demo-oscar-winners/main.js
@@ -1,5 +1,12 @@
 d3.csv("https://raw.githubusercontent.com/miotomita/lede-2022-dataviz/main/01-web-viz/demo-oscar-winners/oscars.csv")
     .then(data => {
-        const years = data.filter(item => item.winner === "1").map(item => item.year)
+        const years = data.filter(item => item.winner === "1")
+            .map(item => item.year)
+        
         console.log(years)
+
+        d3.select("#year")
+            .selectAll("option.opt")
+            .data(years)
+            .join("option")
     })
